cb21b268d6e8d07ca3e6f95c8bca3c8369fc209d
Andrew Cole <aocole@gmail.com>
2013-01-31 13:36:14 -0800

    Migration should also update table sequence name

--- db/migrate/20130118104401_create_identities.rb
+++ db/migrate/20130118104401_create_identities.rb
@@ -1,7 +1,14 @@
 class CreateIdentities < ActiveRecord::Migration
   def self.up
     execute "delete from users where crypted_password is null"
+
     rename_table :users, :identities
+
+    # In AR 3.2 the id sequence is renamed automatically, but not in 3.0..
+    rename_table :users_id_seq, :identities_id_seq
+    execute "ALTER TABLE identities ALTER COLUMN id SET DEFAULT nextval('public.identities_id_seq'::regclass)"
+    execute "ALTER INDEX users_pkey RENAME TO identities_pkey"
+
     rename_column :identities, :crypted_password, :password_digest
     change_column :identities, :password_digest, :string, :null => false
     add_index :identities, :email, :unique => true
