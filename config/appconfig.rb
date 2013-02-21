bbe5b261663d1e94e1e632d53001a666409fec10
Andrew Cole <aocole@gmail.com>
2013-02-20 16:05:45 -0800

    bug 97580 AOK database not configured in cluster

--- config/appconfig.rb
+++ config/appconfig.rb
@@ -4,6 +4,11 @@ begin
 
   CCConfig = Kato::Doozer.walk("/proc/cloud_controller/config/**", { :symbolize_keys => true }).first
 
+  AppConfig[:database_environment] = CCConfig[:database_environment]
+  AppConfig[:database_environment][:production][:database]  = 'aok'
+  AppConfig[:database_environment][:development][:database] = 'db/aok.sqlite3'
+  AppConfig[:database_environment][:test][:database]        = 'db/aok.test.sqlite3'
+
 rescue => ex
   $stderr.puts %[FATAL: Exception encountered while loading config: #{ex}\n#{ex.backtrace.join("\n")}]
   exit 1
