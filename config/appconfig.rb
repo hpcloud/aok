0cd6951e973e4c0ee2b8c67b091b4cd4cb379256
Andrew Cole <aocole@gmail.com>
2013-02-21 12:08:18 -0800

    bug 97627 kato export broken for aok_db

--- config/appconfig.rb
+++ config/appconfig.rb
@@ -1,13 +1,11 @@
+require 'active_support/core_ext/hash/deep_merge'
 begin
   Kato::Doozer.client_name ||= "aok"
   AppConfig = Kato::Doozer.walk("/proc/aok/config/**", { :symbolize_keys => true }).first
 
   CCConfig = Kato::Doozer.walk("/proc/cloud_controller/config/**", { :symbolize_keys => true }).first
 
-  AppConfig[:database_environment] = CCConfig[:database_environment]
-  AppConfig[:database_environment][:production][:database]  = 'aok'
-  AppConfig[:database_environment][:development][:database] = 'db/aok.sqlite3'
-  AppConfig[:database_environment][:test][:database]        = 'db/aok.test.sqlite3'
+  db_config = CCConfig[:database_environment].deep_merge(AppConfig[:database_environment])
 
 rescue => ex
   $stderr.puts %[FATAL: Exception encountered while loading config: #{ex}\n#{ex.backtrace.join("\n")}]
