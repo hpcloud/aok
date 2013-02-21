48a589af00d4e7b9281fc2a0f48837c11dd3c297
Andrew Cole <aocole@gmail.com>
2013-02-21 15:13:35 -0800

    Reverting half-assed db fixes

--- config/appconfig.rb
+++ config/appconfig.rb
@@ -1,11 +1,8 @@
-require 'active_support/core_ext/hash/deep_merge'
 begin
   Kato::Doozer.client_name ||= "aok"
   AppConfig = Kato::Doozer.walk("/proc/aok/config/**", { :symbolize_keys => true }).first
 
   CCConfig = Kato::Doozer.walk("/proc/cloud_controller/config/**", { :symbolize_keys => true }).first
-
-  db_config = CCConfig[:database_environment].deep_merge(AppConfig[:database_environment])
 
 rescue => ex
   $stderr.puts %[FATAL: Exception encountered while loading config: #{ex}\n#{ex.backtrace.join("\n")}]
