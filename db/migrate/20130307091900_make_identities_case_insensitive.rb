52d7d08b962ac8279c8ce949a8cf9ba25cd23ad2
Andrew Cole <aocole@gmail.com>
2013-03-26 16:04:37 -0700

    bug 98299 make admins list case-insensitive

--- db/migrate/20130307091900_make_identities_case_insensitive.rb
+++ db/migrate/20130307091900_make_identities_case_insensitive.rb
@@ -1,5 +1,12 @@
 class MakeIdentitiesCaseInsensitive < ActiveRecord::Migration
   def self.up
+    dupes = execute('select lower(email) as te, count(lower(email)) as num from identities group by te having COUNT(lower(email)) > 1').field_values('te')
+    if dupes.size > 0
+      raise "Duplicate users found in the database while changing to\n"+
+      "case-insensitive index. Please de-dupe the following records:\n"+
+      dupes.join("\n")
+    end
+
     execute 'CREATE EXTENSION citext;'
     change_column :identities, :email, :citext, :null => false
   end
--- models/identity.rb
+++ models/identity.rb
@@ -2,5 +2,13 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   validates :email, 
     :uniqueness => { :case_sensitive => false },
     :length => { :maximum => 255 }
+
+  before_validation do
+    self.email = email.strip.downcase if attribute_present?("email")
+  end
+
+  def email=(val)
+    write_attribute :email, val.strip.downcase
+  end
   
 end
