f086553d17ef6313160dba5e4c9dbb7c448619df
Sridhar Ratnakumar <sridharr@activestate.com>
2013-09-06 14:14:28 -0700

    use correct cc_ng config key

--- config/appconfig.rb
+++ config/appconfig.rb
@@ -5,7 +5,7 @@ else
   require "kato/config"
   begin
     AppConfig = Kato::Config.get("aok").symbolize_keys
-    CCConfig = Kato::Config.get("cloud_controller").symbolize_keys
+    CCConfig = Kato::Config.get("cloud_controller_ng").symbolize_keys
   rescue => ex
     $stderr.puts %[FATAL: Exception encountered while loading config: #{ex}\n#{ex.backtrace.join("\n")}]
     exit 1
