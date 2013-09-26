28f9189f22335120bf16861f81c63b8397605a7a
Ingy döt Net <ingy@ingy.net>
2013-09-25 17:01:55 -0700

    Abstract IDs in response data.

--- test/api/rest-api.bash
+++ test/api/rest-api.bash
@@ -5,7 +5,8 @@ api-patch() { api-call PUT "$@"; }
 api-call() {
     local action="$1" url="$2" data="$3"
     format-curl-command
-    "${curl_command[@]}"
+    "${curl_command[@]}" |
+        perl -pe '$h="[0-9a-f]";s/$h{8}-($h{4}-){3}$h{12}/11111111-2222-3333-4444-555555555555/g'
 }
 format-curl-command() {
     curl_command=(
