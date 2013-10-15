8f40cfbf6137ff64c2e9a78371d38f67c64b908a
Ingy döt Net <ingy@ingy.net>
2013-10-15 13:17:36 -0700

    add a teardown to run after test completes

--- test-api/setup.bash
+++ test-api/setup.bash
@@ -15,4 +15,14 @@ source json.bash
 source `dirname $0`/users-data.sh
 source `dirname $0`/groups-data.sh
 
+trap teardown EXIT
+teardown() {
+  # Remove tmp files if tests passed
+  local rc=$?
+  if [ $rc -eq 0 ]; then
+    rm -f HEAD STDOUT STDERR
+  fi
+  return $rc
+}
+
 # vim: set sw=2:
