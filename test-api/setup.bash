388abc5e634e48dfe899071d00a70167f10dbe34
Ingy döt Net <ingy@ingy.net>
2013-10-17 00:44:41 -0700

    Use new version of test-more-bash

--- test-api/setup.bash
+++ test-api/setup.bash
@@ -1,16 +1,15 @@
 # Setup environment for API Tap tests:
 
-INC="$(set -- $PWD/test-api $PWD/ext/*/{bin,lib}; IFS=':'; echo "$*")"
-PATH="$INC:$PATH"
+BPAN_PATH="$(set -- $PWD/test-api $PWD/ext/*/{bin,lib}; IFS=':'; echo "$*")"
+PATH="$BPAN_PATH:$PATH"
 
 AOK_API_URL="http://aok.$VMNAME.local/uaa"
-TEST_TAP_BAIL_OUT_ON_ERROR=1
-TEST_TAP_ERROR_ON_FAILURE=1
+TEST_TAP_FAIL_FAST=1
 
-source bpan
-bpan:include test/more
-source rest-api.bash
-source json.bash
+source bpan --import include
+include test/more
+include rest-api
+include json
 
 source `dirname $0`/users-data.sh
 source `dirname $0`/groups-data.sh
