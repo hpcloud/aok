5c97dc27157aa8a9e99b24cbd4a6ed4d8f0a06af
Andrew Cole <aocole@gmail.com>
2013-11-20 15:39:42 -0800

    Check for UAA-style keys to save heartache

--- config/appconfig.rb
+++ config/appconfig.rb
@@ -1,6 +1,17 @@
 require "kato/config"
+
+def check_config hash
+  hash.each do |key, value|
+    if key.to_s.include?('-')
+      $stderr.puts "WARNING: Config key #{key.inspect} contains a hyphen. AOK-style keys use _ to separate words."
+    end
+    check_config(value) if value.kind_of?(Hash)
+  end
+end
+
 begin
   AppConfig = Kato::Config.get("aok").symbolize_keys
+  check_config(AppConfig)
   CCConfig = Kato::Config.get("cloud_controller_ng").symbolize_keys
 
   AppConfig[:commit_id] = File.read(File.dirname(__FILE__) + '/../.pkg-gitdescribe').strip
