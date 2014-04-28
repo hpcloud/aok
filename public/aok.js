9e9db8b9b79f19857b6a2d09f2992ee10710e4ce
Tyler Power <tylerpowerx@gmail.com>
2014-04-28 14:09:58 +1200

    Add ability to overwrite console settings defaults. Bug 103739.

--- public/aok.js
+++ public/aok.js
@@ -18,7 +18,33 @@ Settings = {
         style: ''
     },
 
-    refreshServerSettings: function (done) {
+    loadDefaults: function (done) {
+        // 1. Load from external location, to support overriding our defaults.
+        // 2. Fall back to loading our defaults
+        var self = this;
+         $.ajax({
+                url: '/static/console_settings.json',
+                dataType: 'text',
+                type: 'GET',
+                async: true,
+                processData: false,
+                cache: false
+            })
+            .done(function (data, textStatus, jqXHR) {
+                var console_settings;
+                try {
+                    var parsed_settings = JSON.parse(jqXHR.responseText),
+                        merged_settings = {};
+                    self.setting_defaults = $.extend(merged_settings, self.setting_defaults, parsed_settings);
+                } catch (e) {}
+                done(null);
+            })
+            .fail(function () {
+                done(null);
+            });
+    },
+
+    loadThemeSettings: function (done) {
 
         var origin = window.location.origin;
         if (!origin) {
@@ -26,7 +52,6 @@ Settings = {
             origin = window.location.protocol + "//"
                 + window.location.hostname
                 + (window.location.port ? ':' + window.location.port : '');
-
         }
 
         var parseResponse = function (jqXHR) {
@@ -85,62 +110,64 @@ $().ready(function () {
     // Note that to support white labeling the body starts off with display:none, we then load settings from srest
     // and update the page before rendering it.
 
-    Settings.refreshServerSettings(function () {
+    Settings.loadDefaults(function(){
+        Settings.loadThemeSettings(function () {
 
-        var settings = Settings.getSettings();
+                var settings = Settings.getSettings();
 
-        // Use the product name to set the window title
-        document.title = (settings.product_name + ' - ' + settings.company_name);
+                // Use the product name to set the window title
+                document.title = (settings.product_name + ' - ' + settings.company_name);
 
-        // Set the favicon
-        $("#favicon").attr("href", settings.product_logo_favicon_url);
+                // Set the favicon
+                $("#favicon").attr("href", settings.product_logo_favicon_url);
 
-        // Set the header logo
-        $('.navbar-brand').attr('src', settings.product_logo_header_url);
+                // Set the header logo
+                $('.navbar-brand').attr('src', settings.product_logo_header_url);
 
-        $('.form-signin-heading').html('Welcome to ' + settings.product_name);
+                $('.form-signin-heading').html('Welcome to ' + settings.product_name);
 
-        // NOTE: apply additional theme colors in this statement
-        $("head").append("<style type=\"text/css\" charset=\"utf-8\"> body{background-color:" + settings.background_color + "}</style>");
+                // NOTE: apply additional theme colors in this statement
+                $("head").append("<style type=\"text/css\" charset=\"utf-8\"> body{background-color:" + settings.background_color + "}</style>");
 
-        // This must be the last theme/styling to be applied
-        if (settings.style) {
-            $("head").append("<style type=\"text/css\" charset=\"utf-8\">" + settings.style + "</style>");
-        }
-
-        // Show the page
-        $('body').css('display', 'block');
-
-        // Update sign in button state when clicked
-        $('.signin-button').click(function () {
-            $('#invalid_credentials').hide();
-            $('.signin-button').button('loading');
-        });
-
-        $('input').first().focus();
-
-        function getParams() {
-            var params = {};
-
-            if (location.search) {
-                var parts = location.search.substring(1).split('&');
-
-                for (var i = 0; i < parts.length; i++) {
-                    var nv = parts[i].split('=');
-                    if (!nv[0]) continue;
-                    params[nv[0]] = nv[1] || true;
+                // This must be the last theme/styling to be applied
+                if (settings.style) {
+                    $("head").append("<style type=\"text/css\" charset=\"utf-8\">" + settings.style + "</style>");
                 }
-            }
 
-            return params;
-        }
+                // Show the page
+                $('body').css('display', 'block');
 
-        var params = getParams();
-        if (params['message']) {
-            if(params['message'] === 'unauthorized'){
-                $('#failed-login-alert').text("The administrator has not granted you access to " + settings.product_name + ".");
-            }
-            $('#failed-login-alert').removeClass('hide');
-        }
+                // Update sign in button state when clicked
+                $('.signin-button').click(function () {
+                    $('#invalid_credentials').hide();
+                    $('.signin-button').button('loading');
+                });
+
+                $('input').first().focus();
+
+                function getParams() {
+                    var params = {};
+
+                    if (location.search) {
+                        var parts = location.search.substring(1).split('&');
+
+                        for (var i = 0; i < parts.length; i++) {
+                            var nv = parts[i].split('=');
+                            if (!nv[0]) continue;
+                            params[nv[0]] = nv[1] || true;
+                        }
+                    }
+
+                    return params;
+                }
+
+                var params = getParams();
+                if (params['message']) {
+                    if(params['message'] === 'unauthorized'){
+                        $('#failed-login-alert').text("The administrator has not granted you access to " + settings.product_name + ".");
+                    }
+                    $('#failed-login-alert').removeClass('hide');
+                }
+            });
     });
 });
