b4e288cb3bed928232231d4763e7496d6af96b98
Andrew Cole <aocole@gmail.com>
2014-06-12 15:37:43 -0700

    Bug 104192 fix open redirect

--- controllers/oauth_controller.rb
+++ controllers/oauth_controller.rb
@@ -194,10 +194,13 @@ class OauthController < ApplicationController
         client = Client.find_by_identifier(req.client_id) || req.bad_request!('Client not found')
         core = Proc.new do
           raise(Aok::Errors::Unauthorized.new) unless identity
-          # TODO: The next line allows clients without pre-registered redirect_uri, which is allowed
-          # by the oauth2 spec, but not directly supported by rack-oauth2, and probably not a good idea
-          # It's allowed here because the UAA java integration tests rely on this behavior.
-          res.redirect_uri = @redirect_uri = req.verify_redirect_uri!(client.redirect_uri || req.redirect_uri)
+          redirect_uri = begin
+            substitute_redirect_uri(client.redirect_uri)
+          rescue
+            logger.error "Couldn't parse redirect uri for #{client.identifier.inspect}. URI was #{client.redirect_uri.inspect}."
+            nil
+          end
+          res.redirect_uri = req.verify_redirect_uri!(redirect_uri)
           scopes = validate_scope(req, client, identity)
           if params[:response_type]
             case req.response_type
@@ -225,6 +228,16 @@ class OauthController < ApplicationController
       end.call(env)
 
       respond(*oauth_resp)
+    end
+
+    # Stored redirect uris contain a placeholder "ENDPOINT" for the host so
+    # that they don't have to be updated during `kato node rename`. We put
+    # together the actual redirect_uri to use here. This also lets us ensure
+    # we don't redirect outside the cluster.
+    def substitute_redirect_uri abstract_uri
+      u = URI.parse abstract_uri
+      u.host = CCConfig[:external_domain]
+      return u.to_s
     end
 
   end
